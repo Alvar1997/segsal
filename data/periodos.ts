238bf15b6e4066ec0881434b8609cd4aabe59380
Alvar1997 <31602575+Alvar1997@users.noreply.github.com>
2025-01-22 16:36:37 -0600

    Update periodos.ts

--- data/periodos.ts
+++ data/periodos.ts
@@ -7,18 +7,23 @@
     ],
     "meses": [
       {
+        "id": "01",
         "mes": "Enero"
       },
       {
+        "id":"02",
         "mes": "Febrero"
       },
       {
+        "id":"03",
         "mes": "Marzo"
       },
       {
+        "id":"04",
         "mes": "Abril"
       },
       {
+        "id":"05",
         "mes": "Mayo"
       }
     ]
