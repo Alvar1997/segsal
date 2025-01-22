c9544b5f9186f689a87472cc9bb52d67e839d51b
Alvar1997 <31602575+Alvar1997@users.noreply.github.com>
2025-01-22 15:13:38 -0600

    Create unidad.ts

---
+++ data/unidad.ts
@@ -0,0 +1,60 @@
+{
+  "unidades": [
+    {
+      "une_id": "003",
+      "une_desc": "Areas de Staff"
+    },
+    {
+      "une_id": "01-1",
+      "une_desc": "Fundicion de Plomo"
+    },
+    {
+      "une_id": "01-2",
+      "une_desc": "Electrol?tica de  Zinc"
+    },
+    {
+      "une_id": "02",
+      "une_desc": "Refineria"
+    },
+    {
+      "une_id": "03",
+      "une_desc": "Bermejillo"
+    },
+    {
+      "une_id": "096",
+      "une_desc": "Salinas del Rey"
+    },
+    {
+      "une_id": "109",
+      "une_desc": "Aleazin"
+    },
+    {
+      "une_id": "LAB",
+      "une_desc": "Laboratorio Central"
+    },
+    {
+      "une_id": "TAG",
+      "une_desc": "Tratamiento de Aguas"
+    },
+    {
+      "une_id": "902",
+      "une_desc": "Fuerza Eolica"
+    },
+    {
+      "une_id": "GP801",
+      "une_desc": "CIDT"
+    },
+    {
+      "une_id": "GP206",
+      "une_desc": "Comercializadora Metales Pe?oles"
+    },
+    {
+      "une_id": "88",
+      "une_desc": "Fertirey"
+    },
+    {
+      "une_id": "GP341",
+      "une_desc": "Dep?sito Sur"
+    }
+  ]
+}
