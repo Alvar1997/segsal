145e1fd5a916efeb405a23939bf601d053005364
Alvar1997 <31602575+Alvar1997@users.noreply.github.com>
2025-01-22 17:10:38 -0600

    Create informe.ts

---
+++ data/informe.ts
@@ -0,0 +1,1211 @@
+[
+  {
+    "unidad": {
+      "id": "109",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 1,
+        "anio": 1
+      },
+      "aap": {
+        "mes": 3,
+        "anio": 3
+      },
+      "a": {
+        "mes": 1,
+        "anio": 1
+      },
+      "cde": {
+        "mes": 6,
+        "anio": 6
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 8
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "109",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 3,
+        "anio": 4
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 5
+      },
+      "a": {
+        "mes": 1,
+        "anio": 2
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 9
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 13
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "109",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 5,
+        "anio": 9
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 7
+      },
+      "a": {
+        "mes": 2,
+        "anio": 4
+      },
+      "cde": {
+        "mes": 8,
+        "anio": 17
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 12,
+        "anio": 25
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "003",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 1,
+        "anio": 1
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 5
+      },
+      "a": {
+        "mes": 8,
+        "anio": 8
+      },
+      "cde": {
+        "mes": 5,
+        "anio": 5
+      },
+      "f": {
+        "mes": 1,
+        "anio": 1
+      },
+      "dias_perdidos": {
+        "mes": 15,
+        "anio": 15
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "003",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 5,
+        "anio": 6
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 7
+      },
+      "a": {
+        "mes": 3,
+        "anio": 11
+      },
+      "cde": {
+        "mes": 2,
+        "anio": 7
+      },
+      "f": {
+        "mes": 1,
+        "anio": 2
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 25
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "003",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 3,
+        "anio": 9
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 13
+      },
+      "a": {
+        "mes": 1,
+        "anio": 12
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 10
+      },
+      "f": {
+        "mes": 2,
+        "anio": 4
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 30
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "03",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 2,
+        "anio": 2
+      },
+      "aap": {
+        "mes": 4,
+        "anio": 4
+      },
+      "a": {
+        "mes": 4,
+        "anio": 4
+      },
+      "cde": {
+        "mes": 4,
+        "anio": 4
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 12,
+        "anio": 12
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "03",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 1,
+        "anio": 3
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 9
+      },
+      "a": {
+        "mes": 2,
+        "anio": 6
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 5
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "anio": 17,
+        "mes": 5
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "03",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 4,
+        "anio": 7
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 11
+      },
+      "a": {
+        "mes": 3,
+        "anio": 9
+      },
+      "cde": {
+        "mes": 5,
+        "anio": 10
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "anio": 25,
+        "mes": 8
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP801",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 3,
+        "anio": 3
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 2
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 1
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 5
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP801",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 5,
+        "anio": 8
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 2
+      },
+      "a": {
+        "mes": 5,
+        "anio": 5
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 4
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 15
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP801",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 3,
+        "anio": 11
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 4
+      },
+      "a": {
+        "mes": 8,
+        "anio": 13
+      },
+      "cde": {
+        "mes": 5,
+        "anio": 9
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 23
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP341",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 1,
+        "anio": 1
+      },
+      "a": {
+        "mes": 2,
+        "anio": 2
+      },
+      "cde": {
+        "mes": 4,
+        "anio": 4
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 8
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP341",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 3,
+        "anio": 3
+      },
+      "aap": {
+        "mes": 1,
+        "anio": 2
+      },
+      "a": {
+        "mes": 5,
+        "anio": 7
+      },
+      "cde": {
+        "mes": 4,
+        "anio": 8
+      },
+      "f": {
+        "mes": 1,
+        "anio": 1
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 18
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "GP341",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 1,
+        "anio": 4
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 2
+      },
+      "a": {
+        "mes": 3,
+        "anio": 10
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 9
+      },
+      "f": {
+        "mes": 0,
+        "anio": 1
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 27
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-2",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 2,
+        "anio": 2
+      },
+      "aap": {
+        "mes": 3,
+        "anio": 3
+      },
+      "a": {
+        "mes": 8,
+        "anio": 8
+      },
+      "cde": {
+        "mes": 7,
+        "anio": 7
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 3,
+        "anio": 3
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-2",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 7,
+        "anio": 9
+      },
+      "aap": {
+        "mes": 3,
+        "anio": 6
+      },
+      "a": {
+        "mes": 5,
+        "anio": 13
+      },
+      "cde": {
+        "mes": 7,
+        "anio": 14
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 13
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-2",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 0,
+        "anio": 9
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 11
+      },
+      "a": {
+        "mes": 2,
+        "anio": 15
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 15
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 21
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "88",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 2,
+        "anio": 2
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 5
+      },
+      "a": {
+        "mes": 8,
+        "anio": 8
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 3
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 8,
+        "anio": 8
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "88",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 6,
+        "anio": 8
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 7
+      },
+      "a": {
+        "mes": 3,
+        "anio": 11
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 6
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 13
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "88",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 2,
+        "anio": 10
+      },
+      "aap": {
+        "mes": 1,
+        "anio": 8
+      },
+      "a": {
+        "mes": 5,
+        "anio": 16
+      },
+      "cde": {
+        "mes": 2,
+        "anio": 8
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 12,
+        "anio": 25
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "902",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "902",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "902",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-1",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 5,
+        "anio": 5
+      },
+      "aap": {
+        "mes": 6,
+        "anio": 6
+      },
+      "a": {
+        "mes": 8,
+        "anio": 8
+      },
+      "cde": {
+        "mes": 9,
+        "anio": 9
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 15,
+        "anio": 15
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-1",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 8,
+        "anio": 13
+      },
+      "aap": {
+        "mes": 3,
+        "anio": 9
+      },
+      "a": {
+        "mes": 2,
+        "anio": 10
+      },
+      "cde": {
+        "mes": 5,
+        "anio": 14
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 1,
+        "anio": 16
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "01-1",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 2,
+        "anio": 15
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 14
+      },
+      "a": {
+        "mes": 1,
+        "anio": 11
+      },
+      "cde": {
+        "mes": 2,
+        "anio": 16
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 26
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "LAB",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 1,
+        "anio": 1
+      },
+      "cde": {
+        "mes": 2,
+        "anio": 2
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 3,
+        "anio": 3
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "LAB",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 1
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 5
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 10,
+        "anio": 13
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "LAB",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 6
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 2,
+        "anio": 15
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "02",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 18,
+        "anio": 18
+      },
+      "aap": {
+        "mes": 34,
+        "anio": 34
+      },
+      "a": {
+        "mes": 20,
+        "anio": 20
+      },
+      "cde": {
+        "mes": 15,
+        "anio": 15
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 20,
+        "anio": 20
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "02",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 5,
+        "anio": 23
+      },
+      "aap": {
+        "mes": 5,
+        "anio": 39
+      },
+      "a": {
+        "mes": 2,
+        "anio": 22
+      },
+      "cde": {
+        "mes": 3,
+        "anio": 18
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 3,
+        "anio": 23
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "02",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 2,
+        "anio": 25
+      },
+      "aap": {
+        "mes": 2,
+        "anio": 41
+      },
+      "a": {
+        "mes": 0,
+        "anio": 22
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 19
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 28
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "096",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "096",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "096",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 0
+      },
+      "a": {
+        "mes": 0,
+        "anio": 0
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 0
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 0,
+        "anio": 0
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "TAG",
+      "anio": "2025",
+      "mes": "01",
+      "caap": {
+        "mes": 1,
+        "anio": 1
+      },
+      "aap": {
+        "mes": 3,
+        "anio": 3
+      },
+      "a": {
+        "mes": 2,
+        "anio": 2
+      },
+      "cde": {
+        "mes": 5,
+        "anio": 5
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 5
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "TAG",
+      "anio": "2025",
+      "mes": "02",
+      "caap": {
+        "mes": 1,
+        "anio": 2
+      },
+      "aap": {
+        "mes": 0,
+        "anio": 3
+      },
+      "a": {
+        "mes": 1,
+        "anio": 3
+      },
+      "cde": {
+        "mes": 0,
+        "anio": 5
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 5,
+        "anio": 10
+      }
+    }
+  },
+  {
+    "unidad": {
+      "id": "TAG",
+      "anio": "2025",
+      "mes": "03",
+      "caap": {
+        "mes": 0,
+        "anio": 2
+      },
+      "aap": {
+        "mes": 1,
+        "anio": 4
+      },
+      "a": {
+        "mes": 2,
+        "anio": 5
+      },
+      "cde": {
+        "mes": 1,
+        "anio": 6
+      },
+      "f": {
+        "mes": 0,
+        "anio": 0
+      },
+      "dias_perdidos": {
+        "mes": 2,
+        "anio": 12
+      }
+    }
+  }
+]
